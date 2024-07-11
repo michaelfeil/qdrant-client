39cdba2797172f66d1c5e7dc50ef542e7ba60b0e
George Panchuk <george.panchuk@qdrant.tech>
2024-07-11 19:23:12 +0300

    new: set idf modifier in fastembed mixin for bm models

--- tests/test_fastembed.py
+++ tests/test_fastembed.py
@@ -1,6 +1,8 @@
 import pytest
 
-from qdrant_client import QdrantClient
+from qdrant_client import QdrantClient, models
+
+from tests.utils import read_version
 
 
 DOCS_EXAMPLE = {
@@ -127,3 +129,34 @@ def test_set_model():
     # Use the initialized model to add documents with vector embeddings
     local_client.add(collection_name=collection_name, **DOCS_EXAMPLE)
     assert local_client.count(collection_name).count == 2
+
+
+def test_idf_models():
+    local_client = QdrantClient(":memory:")
+
+    if not local_client._FASTEMBED_INSTALLED:
+        pytest.skip("FastEmbed is not installed, skipping")
+
+    major, minor, patch, dev = read_version()
+    version_set = major is not None or dev
+
+    if version_set and not dev:
+        if major == 0 or (major == 1 and (minor < 10 or (minor == 10 and patch < 2))):
+            pytest.skip("Works as of version 1.10.2")
+
+    for model_name in ("Qdrant/bm25", "Qdrant/bm42-all-minilm-l6-v2-attentions"):
+        local_client.set_sparse_model(model_name)
+        collection_name = model_name.split("/")[-1].replace("-", "_")
+
+        local_client.add(collection_name=collection_name, **DOCS_EXAMPLE)
+        local_client.query(
+            collection_name=collection_name, query_text="Qdrant and Llama Index integration"
+        )
+
+        collection_info = local_client.get_collection(collection_name=collection_name)
+        vector_name = local_client.get_sparse_vector_field_name()
+        modifier = collection_info.config.params.sparse_vectors[vector_name].modifier
+        assert modifier == models.Modifier.IDF
+
+    # the only sparse model without IDF is SPLADE, however it's too large for tests, so we don't test how non-idf
+    # models work
